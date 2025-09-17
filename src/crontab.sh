d4ea9569c2d26c54e2de3a23b42133a4bcc59e03
MakerTim <makertim96@gmail.com>
2025-09-17 21:49:12 +0200

    fix: hdd's should be able to spin down

--- src/crontab.sh
+++ src/crontab.sh
@@ -1,6 +1,6 @@
 # do daily/weekly/monthly maintenance
 # min   hour    day     month   weekday command
-*       *       *       *       *       (echo "$(date)") > /out/heartbeat
+*       *       *       *       *       (echo "$(date)") >> /proc/1/fd/1
 0       0       *       *       *       (cd /srv; /usr/bin/rsnapshot -c /srv/rsnapshot.conf daily) >> /proc/1/fd/1
 20      0       */7     *       *       (cd /srv; /usr/bin/rsnapshot -c /srv/rsnapshot.conf weekly) >> /proc/1/fd/1
 40      0       1       *       *       (cd /srv; /usr/bin/rsnapshot -c /srv/rsnapshot.conf monthly) >> /proc/1/fd/1
